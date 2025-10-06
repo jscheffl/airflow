ecc1c2371ecede2d041c07314d6c3788c4ca9aab
Jens Scheffler <j_scheffler@gmx.de>
2025-10-06 18:10:00 +0200

    Enable dynamic generation of assets for Edge UI Plugin

--- dev/breeze/src/airflow_breeze/utils/run_utils.py
+++ dev/breeze/src/airflow_breeze/utils/run_utils.py
@@ -508,6 +508,7 @@ def run_compile_ui_assets(
     dev: bool,
     run_in_background: bool,
     force_clean: bool,
+    additional_ui_hooks: list[str],
 ):
     if force_clean:
         clean_ui_assets()
@@ -524,6 +525,7 @@ def run_compile_ui_assets(
         "--hook-stage",
         "manual",
         "compile-ui-assets-dev" if dev else "compile-ui-assets",
+        *additional_ui_hooks,
         "--all-files",
         "--verbose",
     ]
